5ef52ad8717800323fe2bfcee6c1bbc170a20bd5
Thiago Sol <thiagosol@ymail.com>
2025-04-12 20:32:36 -0300

    Update db.rs

--- src/db.rs
+++ src/db.rs
@@ -2,12 +2,13 @@ use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 
 pub async fn init_db() -> PgPool {
+    let db_host = std::env::var("SERVER_IP").expect("SERVER_IP must be set");
     let db_user = std::env::var("DB_POSTGRES_USER").expect("DB_POSTGRES_USER must be set");
     let db_pass = std::env::var("DB_POSTGRES_PASS").expect("DB_POSTGRES_PASS must be set");
     
     let database_url = format!(
-        "postgres://{}:{}@thiagosol.com:5432/auction",
-        db_user, db_pass
+        "postgres://{}:{}@{}:5432/auction",
+        db_user, db_pass, db_host
     );
 
     PgPoolOptions::new()
